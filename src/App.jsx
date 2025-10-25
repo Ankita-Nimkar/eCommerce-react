325363c811ebb979d918cfb3683023cc82b7da33
Ankita-Nimkar <ankita.dandwate.nimkar@gmail.com>
2025-10-25 16:14:29 +0530

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import "./App.css";
 import HomePage from "./pages/HomePage.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Checkout from "./pages/Checkout";
+import Orders from "./pages/orders";
+import Tracking from "./pages/Tracking";
+import Header from "./components/Header";
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/tracking" element={<Tracking />} />
       </Routes>
     </>
   );
